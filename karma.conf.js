71a2c4b645e16f1863b17df190b09759410af299
Vee <20609046+bolorundurovj@users.noreply.github.com>
2026-05-30 14:04:58 +0100

    ci: split tests into own jobs, fix audit to omit devDeps, add ChromeHeadlessCI

--- karma.conf.js
+++ karma.conf.js
@@ -31,6 +31,12 @@ module.exports = function (config) {
     },
     reporters: ['progress', 'kjhtml'],
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
+      }
+    },
     restartOnFileChange: true
   });
 };
